b0667e3e86f4ff4f4c89599bc5d99fd618ff0dc8
l.oversloot <l.oversloot@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-10-29 13:38:00 +0000

    Layout van KnoppenView klaar

--- trunk/src/ipse/KnopView.java
+++ trunk/src/ipse/KnopView.java
@@ -5,27 +5,32 @@ import java.awt.event.*;
 
 public class KnopView extends JPanel implements ActionListener
 {
-	private JButton bestellingKnop, artikelKnop, medewerkerKnop, klantenKnop, zoekKnop, uitlogKnop;
+	private JButton bestellingKnop, artikelKnop, medewerkerKnop, klantenKnop, zoekKnop, uitlogKnop,
+					nieuwKnop, bewerkKnop, verwijderKnop, meerInfoKnop;
+	private JLabel logoLabel;
 	
 	public KnopView()
 	{
 		setLayout( null );
 		
+		logoLabel = new JLabel( new ImageIcon("src/ipse/Images/logo.png") );
+		logoLabel.setBounds( 250, 20, 300, 100 );
+		
 		bestellingKnop = new JButton( "Bestelling" );
 		bestellingKnop.addActionListener( this );
-		bestellingKnop.setBounds( 130, 540, 100, 30 );
+		bestellingKnop.setBounds( 170, 540, 100, 30 );
 		
 		artikelKnop = new JButton( "Artikel" );
 		artikelKnop.addActionListener( this );
-		artikelKnop.setBounds( 260, 540, 100, 30 );
+		artikelKnop.setBounds( 300, 540, 100, 30 );
 		
 		medewerkerKnop = new JButton( "Medewerker" );
 		medewerkerKnop.addActionListener( this );
-		medewerkerKnop.setBounds( 390, 540, 100, 30 );
+		medewerkerKnop.setBounds( 430, 540, 100, 30 );
 		
 		klantenKnop = new JButton( "Klanten" );
 		klantenKnop.addActionListener( this );
-		klantenKnop.setBounds( 520, 540, 100, 30 );
+		klantenKnop.setBounds( 560, 540, 100, 30 );
 		
 		zoekKnop = new JButton( "Zoek" );
 		zoekKnop.addActionListener( this );
@@ -33,13 +38,35 @@ public class KnopView extends JPanel implements ActionListener
 		
 		uitlogKnop = new JButton( "Uitloggen" );
 		uitlogKnop.addActionListener( this );
+		uitlogKnop.setBounds( 30, 20, 100, 30 );
 		
+		nieuwKnop = new JButton( "Nieuw" );
+		nieuwKnop.addActionListener( this );
+		nieuwKnop.setBounds( 10, 210, 100, 30 );
+		
+		bewerkKnop = new JButton( "Bewerk" );
+		bewerkKnop.addActionListener( this );
+		bewerkKnop.setBounds( 10, 260, 100, 30 );
+		
+		verwijderKnop = new JButton( "Verwijder" );
+		verwijderKnop.addActionListener( this );
+		verwijderKnop.setBounds( 10, 310, 100, 30 );
+		
+		meerInfoKnop = new JButton( "Meer Info" );
+		meerInfoKnop.addActionListener( this );
+		meerInfoKnop.setBounds( 10, 360, 100, 30 );
+		
+		add( logoLabel );
 		add( bestellingKnop );
 		add( artikelKnop );
 		add( medewerkerKnop );
 		add( klantenKnop );
 		add( zoekKnop );
 		add( uitlogKnop );
+		add( nieuwKnop );
+		add( bewerkKnop );
+		add( verwijderKnop );
+		add( meerInfoKnop );
 		
 	}
 
