7e43d4e795017dc261040bb710fa0482fbad2ad6
l.oversloot <l.oversloot@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-10-29 11:11:07 +0000

    Visulele deel van Login

--- src/ipse/LoginView.java
+++ src/ipse/LoginView.java
@@ -7,40 +7,53 @@ import java.awt.event.*;
 public class LoginView extends JFrame implements ActionListener
 {
 	private JButton okKnop, annulerenKnop, vergetenKnop;
-	private JTextField inlogNaamVeld, wachtWoordVeld;
-	private JLabel imageLabel;
+	private JTextField inlogNaamVeld;
+	private JPasswordField wwVeld;
+	private JLabel imageLabel, inlogLabel, wwLabel;
+	private ImageIcon login;
 	
 	public LoginView()
 	{
 		setLayout( null );
 		
-		setSize( 800,600 );
+		setSize( 400,300 );
 		setLocation( 100,100 );
 		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		setTitle( "BestelSnel Login" );
 		
 		inlogNaamVeld = new JTextField( 10 );
-		wachtWoordVeld = new JTextField( 10 );
+		inlogNaamVeld.setBounds( 170, 110, 130, 20 );
+		inlogLabel = new JLabel( "Inlognaam" );
+		inlogLabel.setBounds( 90, 110, 90, 20 );
+		
+		wwVeld = new JPasswordField( 10 );
+		wwVeld.setBounds( 170, 140, 130, 20 );
+		wwLabel = new JLabel( "Wachtwoord" );
+		wwLabel.setBounds( 90, 140, 90, 20 );
 		
 		okKnop = new JButton( "OK" );
 		okKnop.addActionListener( this );
+		okKnop.setBounds( 90, 200, 100, 50 );
 		
 		annulerenKnop = new JButton( "Annuleren" );
 		annulerenKnop.addActionListener( this );
+		annulerenKnop.setBounds( 195, 200, 100, 50 );
 		
 		vergetenKnop = new JButton( "Wachtwoord vergeten?" );
 		vergetenKnop.addActionListener( this );
 	
-		imageLabel = new JLabel( new ImageIcon( "/ipse2/src/ipse/Images/logo.png" ) );
-		imageLabel.setBorder(BorderFactory.createLineBorder(Color.black));
-		imageLabel.setBounds( 10, 10, 200, 100 );
+		login = new ImageIcon( "src/ipse/Images/logo.png" );
+		imageLabel = new JLabel( login );
+		imageLabel.setBounds( 50, 0, 300, 100 );
 		
 		add( okKnop );
 		add( annulerenKnop );
 		add( vergetenKnop );
 		add( inlogNaamVeld );
-		add( wachtWoordVeld );
+		add( wwVeld );
 		add( imageLabel );
+		add( inlogLabel );
+		add( wwLabel );
 		
 		setVisible( true );
 	}
