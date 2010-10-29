2f5bc9eaec0016f759a0094b4758e8ab9fe67af6
lennardschoemaker <lennardschoemaker@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-10-29 13:42:57 +0000

    Begin zoek-Klasse

--- trunk/src/ipse/Zoek.java
+++ trunk/src/ipse/Zoek.java
@@ -1,5 +1,22 @@
 package ipse;
 
-public class Zoek {
+import javax.swing.*;
+import java.util.*;
+import java.awt.event.*;
 
+public class Zoek extends JFrame implements ActionListener
+{
+	
+
+	public Zoek()
+	{
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
