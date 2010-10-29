ce1485ad8d6a5a344720bab78396c0a2b7006f6f
l.oversloot <l.oversloot@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-10-29 09:13:51 +0000

    Eerste gedeelte opstartklasse

--- src/ipse/BestelSnelApp.java
+++ src/ipse/BestelSnelApp.java
@@ -1,5 +1,21 @@
 package ipse;
 
-public class BestelSnelApp {
+import javax.swing.*;
 
+public class BestelSnelApp extends JFrame
+{
+	public BestelSnelApp()
+	{
+		this.setSize( 800, 600 );
+		this.setLocation( 100,100 );
+		this.setTitle( "BestelSnel Groep1" );
+		this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		
+		this.setVisible( true );
+	}
+	
+	public static void main( String[]args )
+	{
+		new BestelSnelApp();
+	}
 }
