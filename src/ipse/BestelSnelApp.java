f677fe91ce610d3d3a1eb3a4b609a8e8fc18e0d5
l.oversloot <l.oversloot@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-10-29 12:37:36 +0000

    Toch de knoppen anders

--- src/ipse/BestelSnelApp.java
+++ src/ipse/BestelSnelApp.java
@@ -7,6 +7,8 @@ public class BestelSnelApp extends JFrame
 {
 	public BestelSnelApp()
 	{
+		this.setLayout( new BorderLayout() );
+		
 		this.setSize( 800, 600 );
 		this.setLocation( 100,100 );
 		this.setTitle( "BestelSnel Groep1" );
@@ -14,20 +16,19 @@ public class BestelSnelApp extends JFrame
 		
 		NoordPaneel noordPaneel = new NoordPaneel();
 		WestPaneel westPaneel = new WestPaneel();
+		OostPaneel oostPaneel = new OostPaneel();
 		ZuidPaneel zuidPaneel = new ZuidPaneel();
 		
 		add(noordPaneel, BorderLayout.NORTH);
 		add(westPaneel, BorderLayout.WEST);
+		add(oostPaneel, BorderLayout.EAST );
 		add(zuidPaneel, BorderLayout.SOUTH);
-		
-		//JPanel knopView = new KnopView();
-		//this.add( knopView );
 		
 		this.setVisible( true );
 	}
 	
 	public static void main( String[]args )
 	{
-		new LoginView();
+		new BestelSnelApp();
 	}
 }
