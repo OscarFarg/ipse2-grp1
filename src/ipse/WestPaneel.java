892f8022be826fd2aea1e22882198fedd548a66e
l.oversloot <l.oversloot@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-10-29 12:09:35 +0000

    westpaneel testing

--- src/ipse/WestPaneel.java
+++ src/ipse/WestPaneel.java
@@ -1,34 +1,39 @@
 package ipse;
 
 import javax.swing.*;
+import java.awt.Color;
 import java.awt.event.*;
 
 public class WestPaneel extends JPanel implements ActionListener
 {
-	private JButton nieuwKnop, bewerkKnop, verwijderKnop, openKnop;
+	private JButton nieuwKnop, bewerkKnop, verwijderKnop, openKnop, testKnop;
 	
 	public WestPaneel()
-	{
+	{	
 		//setLayout(null);
+		setBackground( Color.red );
 		
 		nieuwKnop = new JButton("Nieuw");
 		nieuwKnop.addActionListener(this);
-		//nieuwKnop.setBounds(, , , );
+		//nieuwKnop.setBounds( 10, 0, 100, 20 );
 		add(nieuwKnop);
+		
+		testKnop = new JButton( "test" );
+		add(testKnop);
 		
 		bewerkKnop = new JButton("Bewerk");
 		bewerkKnop.addActionListener(this);
-		//bewerkKnop.setBounds(, , , );
+		//bewerkKnop.setBounds( 10, 70, 100, 20 );
 		add(bewerkKnop);
 		
 		verwijderKnop = new JButton("Verwijder");
 		verwijderKnop.addActionListener(this);
-		//verwijderKnop.setBounds(, , , );
+		//verwijderKnop.setBounds( 10, 140, 100, 20 );
 		add(verwijderKnop);
 		
 		openKnop = new JButton("Open");
 		openKnop.addActionListener(this);
-		//openKnop.setBounds(, , , );
+		//openKnop.setBounds( 10, 210, 100, 20 );
 		add(openKnop);
 	}
 
