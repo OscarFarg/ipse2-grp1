764bc727a7aa5c98968e0280767bb50f01091e90
l.oversloot <l.oversloot@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-10-30 14:34:53 +0000

    MedewerkerView gemaakt aan de hand van bestellingView

--- src/ipse/MedewerkerView.java
+++ src/ipse/MedewerkerView.java
@@ -1,5 +1,57 @@
 package ipse;
 
-public class MedewerkerView {
+import java.sql.*;
+import java.util.*;
 
+import javax.swing.*;
+
+public class MedewerkerView extends View
+{
+	private Database database;
+	private Controller controller;
+	private JTable medewerkers;
+
+	public MedewerkerView( Database database, Controller controller )
+	{
+		this.database = database;
+		this.controller = controller;
+
+		maakLijst();
+
+		JScrollPane scrollPane = new JScrollPane(medewerkers);
+		add(scrollPane);
+	}
+
+	public void maakLijst()
+	{
+		ResultSet resultSet = database.getMedewekers();
+
+		ResultSetMetaData metaData = resultSet.getMetaData();
+		int numberOfColumns = metaData.getColumnCount();
+
+		Vector<String> koppen = new Vector<String>();
+		for (int kolom = 1; kolom <= numberOfColumns; kolom++)
+		{
+			koppen.add(metaData.getColumnName(kolom));
+		}
+
+		Vector<Vector<String>> data = new Vector<Vector<String>>();
+		while (resultSet.next())
+		{
+			Vector<String> rij = new Vector<String>();
+			for (int kolom = 1; kolom <= numberOfColumns; kolom++)
+			{
+				rij.add(resultSet.getString(kolom));
+			}
+			data.add(rij);
+		}
+		medewerkers = new JTable(data, koppen)
+		{
+			public boolean isCellEditable(int rowIndex, int mColIndex)
+			{
+				return false;
+			}
+		};
+		medewerkers.setAutoCreateRowSorter(true);
+	}
 }
