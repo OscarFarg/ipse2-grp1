6055ed34543d29b9c95ab49c29c3fea42bb48fb7
lennardschoemaker <lennardschoemaker@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-10-31 17:59:23 +0000

    Artikel attributed add and getters

--- trunk/src/ipse/Artikel.java
+++ trunk/src/ipse/Artikel.java
@@ -1,5 +1,36 @@
 package ipse;
 
-public class Artikel {
+public class Artikel 
+{
+	private int artikelid;
+	private String artikelNaam;
+	private double prijs;
+	
+	
+	public Artikel( int artikelid, String artikelNaam, double prijs)
+	{
+		this.artikelid = artikelid;
+		this.artikelNaam = artikelNaam;
+		this.prijs = prijs;
+	}
 
+
+	public int getArtikelid() 
+	{
+		return artikelid;
+	}
+
+
+	public String getArtikelNaam() 
+	{
+		return artikelNaam;
+	}
+
+
+	public double getPrijs() 
+	{
+		return prijs;
+	}
+	
+	
 }
