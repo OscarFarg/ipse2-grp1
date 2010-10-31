0f590dce765ba5d9df7e1ffd8af3c76cd2932869
lennardschoemaker <lennardschoemaker@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-10-31 17:11:36 +0000

    1e versie van het zoekframe, inclusief het toevoegen van functie aan de zoekKnop op het buttonview

--- trunk/src/ipse/Zoek.java
+++ trunk/src/ipse/Zoek.java
@@ -6,16 +6,106 @@ import java.awt.event.*;
 
 public class Zoek extends JFrame implements ActionListener
 {
+	private JRadioButton artikelRadio;
+	private JRadioButton bestellingRadio;
+	private JRadioButton bestelregelRadio;
+	private JRadioButton klantRadio;
+	private JRadioButton medewerkerRadio;
+	
+	private JButton zoekKnop;
+	
+	private JTextField zoekVeld;
+	
+	private String artikelString = "Artikel";
+	private String bestellingString = "Besteling";
+	private String bestelregelString = "Bestelregel";
+	private String klantString = "Klant";
+	private String medewerkerString = "Medewerker";
+	
+	private String zoekSegment = "Artikel"; // Bepaald waarop wordt gezocht
 	
 
 	public Zoek()
-	{
+	{		
+		JPanel contentPane = new JPanel();
+		JFrame venster = new JFrame();
+		venster.setSize(600,300); 
+		venster.setTitle("ZoekVenster");
+		venster.setLocation(300,300);
+		venster.setContentPane(contentPane);
+		venster.setVisible(true);
+		
+		artikelRadio = new JRadioButton(artikelString);
+		bestellingRadio = new JRadioButton(bestellingString);
+		bestelregelRadio = new JRadioButton(bestelregelString);
+		klantRadio = new JRadioButton(klantString);
+		medewerkerRadio = new JRadioButton(medewerkerString);
+		
+		artikelRadio.setSelected(true);
+		
+		zoekKnop = new JButton("Zoek");
+		
+		zoekVeld = new JTextField("zoekwoord", 20);
+		
+		contentPane.add(new JLabel("Kies hier uw zoekoptie."));
+		contentPane.add(artikelRadio);
+		contentPane.add(bestellingRadio);
+		contentPane.add(bestelregelRadio);
+		contentPane.add(klantRadio);
+		contentPane.add(medewerkerRadio);
+		contentPane.add( zoekVeld);
+		contentPane.add(zoekKnop);
+	
+		ButtonGroup group = new ButtonGroup();
+		group.add(artikelRadio);
+		group.add(bestellingRadio);
+		group.add(bestelregelRadio);
+		group.add(klantRadio);
+		group.add(medewerkerRadio);
+		
+		artikelRadio.addActionListener(this);
+		bestellingRadio.addActionListener(this);
+		bestelregelRadio.addActionListener(this);
+		klantRadio.addActionListener(this);
+		medewerkerRadio.addActionListener(this);
+		zoekKnop.addActionListener(this);
 		
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	
+	public void actionPerformed(ActionEvent e) 
+	{
+		if(e.getSource() == artikelRadio )
+		{
+			zoekSegment = artikelString;
+		}
+		
+		if(e.getSource() == bestellingRadio )
+		{
+			zoekSegment = bestellingString;
+		}
+		
+		if(e.getSource() == bestelregelRadio )
+		{
+			zoekSegment = bestelregelString;
+		}
+		
+		if(e.getSource() == klantRadio )
+		{
+			zoekSegment = klantString;
+		}
+		
+		if(e.getSource() == medewerkerRadio )
+		{
+			zoekSegment = medewerkerString;
+		}
+		
+		if(e.getSource() == zoekKnop )
+		{
+			// Hier moet Query of methode komen uit andere klasse voor het doorzoeken van de database
+			
+		}
+		
 		
 	}
 	
