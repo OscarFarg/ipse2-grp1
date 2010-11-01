401ed7483a74c12860e4e264da16560ac3063127
ozziej@gmail.com <ozziej@gmail.com@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-11-01 12:10:59 +0000

    Klant basis

--- src/ipse/Klant.java
+++ src/ipse/Klant.java
@@ -1,5 +1,55 @@
 package ipse;
 
 public class Klant {
+	private String id;
+	private String voornaam;
+	private String tussenvoegsel;
+	private String rekeningNr;
+	private String betaalStatus;
+	private String klantStatus;
+	
+	public Klant()
+	{
+		
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getVoornaam() {
+		return voornaam;
+	}
+	public void setVoornaam(String voornaam) {
+		this.voornaam = voornaam;
+	}
+	public String getTussenvoegsel() {
+		return tussenvoegsel;
+	}
+	public void setTussenvoegsel(String tussenvoegsel) {
+		this.tussenvoegsel = tussenvoegsel;
+	}
+	public String getRekeningNr() {
+		return rekeningNr;
+	}
+	public void setRekeningNr(String rekeningNr) {
+		this.rekeningNr = rekeningNr;
+	}
+	public String getBetaalStatus() {
+		return betaalStatus;
+	}
+	public void setBetaalStatus(String betaalStatus) {
+		this.betaalStatus = betaalStatus;
+	}
+	public String getKlantStatus() {
+		return klantStatus;
+	}
+	public void setKlantStatus(String klantStatus) {
+		this.klantStatus = klantStatus;
+	}
+	
+	
 
 }
