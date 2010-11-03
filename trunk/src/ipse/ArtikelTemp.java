bc059df7190011d6b9f919e1a4ad43b7778e2f34
l.oversloot <l.oversloot@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-11-03 12:58:19 +0000

    lay-out artikel toevoegen/bewerken

--- trunk/src/ipse/ArtikelTemp.java
+++ trunk/src/ipse/ArtikelTemp.java
@@ -5,8 +5,8 @@ import java.awt.event.*;
 
 public class ArtikelTemp extends JFrame implements ActionListener
 {
-	private JTextField artikelNaamVeld, artikelPrijsVeld;
-	private JLabel imageLabel, bsLabel, artikelNaamLabel, artikelPrijsLabel;
+	private JTextField artikelIdVeld, artikelNaamVeld, artikelPrijsVeld;
+	private JLabel imageLabel, bsLabel, artikelIdLabel, artikelNaamLabel, artikelPrijsLabel;
 	private JButton voegToeKnop;
 	private ImageIcon logoIcon;
 
@@ -15,6 +15,7 @@ public class ArtikelTemp extends JFrame implements ActionListener
 		setSize( 600,400 );
 		setLocation( 100,100 );
 		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		setResizable( false );
 		
 		JPanel mainPanel = new JPanel();
 		setContentPane( mainPanel );
@@ -28,24 +29,34 @@ public class ArtikelTemp extends JFrame implements ActionListener
 		bsLabel = new JLabel( "Voeg hier onder een nieuw artikel toe" );
 		bsLabel.setBounds( 185, 120, 250, 20 );
 		
+		artikelIdLabel = new JLabel( "Artikel ID" );
+		artikelIdLabel.setBounds( 150, 160, 100, 20 );
+		
+		artikelIdVeld = new JTextField( 5 );
+		artikelIdVeld.setBounds( 250, 160, 200, 20 );
+		artikelIdVeld.setEditable( false );
+		
 		artikelNaamLabel = new JLabel( "Artikel Naam" );
-		artikelNaamLabel.setBounds( 150, 160, 100, 20 );
+		artikelNaamLabel.setBounds( 150, 190, 100, 20 );
 		
 		artikelNaamVeld = new JTextField( 20 );
-		artikelNaamVeld.setBounds( 250, 160, 200, 20 );
+		artikelNaamVeld.setBounds( 250, 190, 200, 20 );
 		
 		artikelPrijsLabel = new JLabel( "Artikel Prijs" );
-		artikelPrijsLabel.setBounds( 150, 190, 100, 20 );
+		artikelPrijsLabel.setBounds( 150, 220, 100, 20 );
 		
 		artikelPrijsVeld = new JTextField( 5 );
-		artikelPrijsVeld.setBounds( 250, 190, 200, 20 );
+		artikelPrijsVeld.setBounds( 250, 220, 200, 20 );
 		
 		voegToeKnop = new JButton( "Voeg Toe" );
-		voegToeKnop.setBounds( 350, 230, 100, 30 );
+		voegToeKnop.setBounds( 350, 250, 100, 30 );
 		
+		mainPanel.add( imageLabel );
 		mainPanel.add( bsLabel );
+		mainPanel.add( artikelIdVeld );
 		mainPanel.add( artikelNaamVeld );
 		mainPanel.add( artikelPrijsVeld );
+		mainPanel.add( artikelIdLabel );
 		mainPanel.add( artikelNaamLabel );
 		mainPanel.add( artikelPrijsLabel );
 		mainPanel.add( voegToeKnop );
