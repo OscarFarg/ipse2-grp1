e8d46cbdf106df121cf7829b0c0fc386134a7f2b
ozziej@gmail.com <ozziej@gmail.com@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-11-04 11:55:16 +0000

    Opslaan + Annuleren in de actionperformed gezet

--- trunk/src/ipse/View.java
+++ trunk/src/ipse/View.java
@@ -9,55 +9,58 @@ public class View extends JFrame implements ActionListener
 {
 	//protected zodat die in alle subklasses gebruikt kan worden.
 	protected JPanel mainPanel;
-	
+
 	private JButton opslaanKnop, annulerenKnop;
-	
+
 	Database database;
 	Controller controller;
-	
+
 	public View( Database database, Controller controller )
 	{
 		this.database = database;
 		this.controller = controller;
-		
+
 		ImageIcon logoIcon = new ImageIcon("src/ipse/images/logo.png");
 		JLabel imageLabel = new JLabel( logoIcon );
 		imageLabel.setBounds( 150, 10, 300, 100 );
-		
+
 		//De knoppen voor alle views
-		
+
 		opslaanKnop = new JButton( "Opslaan" );
 		opslaanKnop.addActionListener( this );
 		opslaanKnop.setBounds( 350, 250, 100, 30 );
-		
+
 		annulerenKnop = new JButton( "Annuleren" );
 		annulerenKnop.addActionListener( this );
 		annulerenKnop.setBounds( 460, 250, 100, 30 );
-		
+
 		//Dit geld voor alle views.
 		setSize( 600,400 );
 		setLocationRelativeTo(null);
 		//setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		setResizable( false );
-		
+
 		mainPanel = new JPanel();
 		setContentPane( mainPanel );
-		
+
 		mainPanel.add( imageLabel );
 		mainPanel.add( opslaanKnop );
 		mainPanel.add( annulerenKnop );
-		
+
 		mainPanel.setLayout( null );
-		
+
 	}
-	
+
 	public void opslaan(){}
-	
+
 	public void annuleren(){}
 
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
-		
+		if (e.getSource() == opslaanKnop)
+			opslaan();
+		else if (e.getSource() == annulerenKnop)
+			annuleren();
 	}
 }
