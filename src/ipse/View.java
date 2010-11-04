48474f1b5debacb5f8023fec9a6dd6143c005a05
l.oversloot <l.oversloot@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-11-04 11:51:48 +0000

    knoppen toegevoegd, en opslaan en annuleren methode toegevoegd

--- src/ipse/View.java
+++ src/ipse/View.java
@@ -10,17 +10,29 @@ public class View extends JFrame implements ActionListener
 	//protected zodat die in alle subklasses gebruikt kan worden.
 	protected JPanel mainPanel;
 	
+	private JButton opslaanKnop, annulerenKnop;
+	
 	Database database;
 	Controller controller;
 	
-	public View()
+	public View( Database database, Controller controller )
 	{
-		database = new Database();
-		controller = new Controller();
+		this.database = database;
+		this.controller = controller;
 		
 		ImageIcon logoIcon = new ImageIcon("src/ipse/images/logo.png");
 		JLabel imageLabel = new JLabel( logoIcon );
 		imageLabel.setBounds( 150, 10, 300, 100 );
+		
+		//De knoppen voor alle views
+		
+		opslaanKnop = new JButton( "Opslaan" );
+		opslaanKnop.addActionListener( this );
+		opslaanKnop.setBounds( 350, 250, 100, 30 );
+		
+		annulerenKnop = new JButton( "Annuleren" );
+		annulerenKnop.addActionListener( this );
+		annulerenKnop.setBounds( 460, 250, 100, 30 );
 		
 		//Dit geld voor alle views.
 		setSize( 600,400 );
@@ -32,14 +44,20 @@ public class View extends JFrame implements ActionListener
 		setContentPane( mainPanel );
 		
 		mainPanel.add( imageLabel );
-
+		mainPanel.add( opslaanKnop );
+		mainPanel.add( annulerenKnop );
 		
 		mainPanel.setLayout( null );
 		
 	}
+	
+	public void opslaan(){}
+	
+	public void annuleren(){}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e) 
+	{
 		
 	}
 }
