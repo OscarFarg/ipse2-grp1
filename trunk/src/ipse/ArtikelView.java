a1e45645f722cf5da986a72f5b2ce0806b267436
l.oversloot <l.oversloot@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-11-04 13:35:25 +0000

    foutafhandeling bij foute invoer nieuw artikel

--- trunk/src/ipse/ArtikelView.java
+++ trunk/src/ipse/ArtikelView.java
@@ -11,29 +11,29 @@ public class ArtikelView extends View
 	public ArtikelView( Database database, Controller controller )
 	{
 		super( database, controller );
-		
+
 		bsLabel = new JLabel( "Voeg hier onder een nieuw artikel toe" );
 		bsLabel.setBounds( 185, 120, 250, 20 );
-		
+
 		artikelIdLabel = new JLabel( "Artikel ID" );
 		artikelIdLabel.setBounds( 150, 160, 100, 20 );
-		
+
 		artikelIdVeld = new JTextField( 5 );
 		artikelIdVeld.setBounds( 250, 160, 200, 20 );
 		artikelIdVeld.setEditable( false );
-		
+
 		artikelNaamLabel = new JLabel( "Artikel Naam" );
 		artikelNaamLabel.setBounds( 150, 190, 100, 20 );
-		
+
 		artikelNaamVeld = new JTextField( 20 );
 		artikelNaamVeld.setBounds( 250, 190, 200, 20 );
-		
+
 		artikelPrijsLabel = new JLabel( "Artikel Prijs" );
 		artikelPrijsLabel.setBounds( 150, 220, 100, 20 );
-		
+
 		artikelPrijsVeld = new JTextField( 5 );
 		artikelPrijsVeld.setBounds( 250, 220, 200, 20 );
-		
+
 		mainPanel.add( bsLabel );
 		mainPanel.add( artikelIdVeld );
 		mainPanel.add( artikelNaamVeld );
@@ -41,8 +41,8 @@ public class ArtikelView extends View
 		mainPanel.add( artikelIdLabel );
 		mainPanel.add( artikelNaamLabel );
 		mainPanel.add( artikelPrijsLabel );
-	
-		
+
+
 		setVisible( true );
 	}
 
@@ -52,21 +52,24 @@ public class ArtikelView extends View
 		double prijs = Double.parseDouble( artikelPrijsVeld.getText() );
 		Artikel artikel = new Artikel( naam, prijs );
 		database.insertArtikel(artikel);
-		
-		int n = JOptionPane.showConfirmDialog(null, "Artikel is toegevoegd!, Wilt u er nog een toevoegen?",
-				"Toevoegen", JOptionPane.YES_NO_OPTION);
-		if (n == 0) 
+
+		if( naam.length() != 0 && prijs >= 0.0 )
 		{
-			artikelNaamVeld.setText("");
-			artikelPrijsVeld.setText("");
-		} 
-		else {
-			this.dispose();
+			int n = JOptionPane.showConfirmDialog(null, "Artikel is toegevoegd!, Wilt u er nog een toevoegen?",
+					"Toevoegen", JOptionPane.YES_NO_OPTION);
+			if (n == 0) 
+			{
+				artikelNaamVeld.setText("");
+				artikelPrijsVeld.setText("");
+			} 
+			else {
+				this.dispose();
+			}
 		}
-	}
-	
-	public void annuleren()
-	{
-		this.dispose();
+		else
+		{
+			JOptionPane.showMessageDialog(null, "De ingevoerde waarden kloppen niet. Controleer of alles is ingevuld en of de prijs hoger is dan 0.0", 
+					"Fout", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 }
