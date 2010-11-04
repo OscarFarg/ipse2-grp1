5c33cf09fcb8d05531142abb06bef6a457541088
ozziej@gmail.com <ozziej@gmail.com@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-11-04 11:00:31 +0000

    View aangepast en ArtikelView subklasse van View gemaakt.

--- trunk/src/ipse/KlantView.java
+++ trunk/src/ipse/KlantView.java
@@ -1,5 +1,15 @@
 package ipse;
 
-public class KlantView extends View{
+public class KlantView extends View {
 	
+	public KlantView()
+	{
+		this.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		new KlantView();
+	}
 }
--- trunk/src/ipse/View.java
+++ trunk/src/ipse/View.java
@@ -1,11 +1,32 @@
 package ipse;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class View extends JPanel
+public class View extends JFrame implements ActionListener
 {
+	//protected zodat die in alle subklasses gebruikt kan worden.
+	protected JPanel mainPanel;
+	
 	public View()
 	{
+		//Dit geld voor alle views.
+		setSize( 600,400 );
+		setLocationRelativeTo(null);
+		//setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		setResizable( false );
+		
+		mainPanel = new JPanel();
+		setContentPane( mainPanel );
+		
+		mainPanel.setLayout( null );
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		
 	}
 }
