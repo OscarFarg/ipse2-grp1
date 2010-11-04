12a29b4d863c613911ee648c775c12887db822d2
theojr.moorman@gmail.com <theojr.moorman@gmail.com@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-11-04 13:12:40 +0000

    klantview, nog zonder acties knopview verwijderd

--- src/ipse/KlantView.java
+++ src/ipse/KlantView.java
@@ -1,15 +1,76 @@
 package ipse;
 
-public class KlantView extends View {
+import javax.swing.*;
+
+public class KlantView extends View
+{
+	private JTextField idVeld, voornaamVeld, tussenvoegselVeld, achternaamVeld, rekeningVeld;
+	private JComboBox betaalStatusBox;
 	
-	public KlantView()
+	public KlantView(Database database, Controller controller)
 	{
+		super(database, controller);
+		
+		//Id
+		idVeld = new JTextField(5);
+		idVeld.setEditable(false);
+		idVeld.setBounds( 150, 110, 100, 20 );
+		JLabel idLabel = new JLabel("Klant ID");
+		idLabel.setBounds(40, 110, 120, 20);
+		
+		//Voornaam
+		voornaamVeld = new JTextField(15);
+		voornaamVeld.setBounds(150, 135, 150, 20);
+		JLabel voornaamLabel = new JLabel("Voornaam");
+		voornaamLabel.setBounds(40, 135, 120, 20);
+		
+		tussenvoegselVeld = new JTextField(10);
+		tussenvoegselVeld.setBounds(150, 160, 150, 20);
+		JLabel tussenvoegselLabel = new JLabel("Tussenvoegsel");
+		tussenvoegselLabel.setBounds(40, 160, 120, 20);
+
+		achternaamVeld = new JTextField(15);
+		achternaamVeld.setBounds(150, 185, 150, 20);
+		JLabel achternaamLabel = new JLabel("Achternaam");
+		achternaamLabel.setBounds(40, 185, 120, 20);
+
+		rekeningVeld = new JTextField(15);
+		rekeningVeld.setBounds(150, 210, 150, 20);
+		JLabel rekeningnrLabel = new JLabel("Rekening");
+		rekeningnrLabel.setBounds(40, 210, 120, 20);
+
+		betaalStatusBox = new JComboBox();
+		betaalStatusBox.addItem("Goede betaler");
+		betaalStatusBox.addItem("Slechte betaler");
+		betaalStatusBox.addItem("Gemiddelde betaler");
+		betaalStatusBox.setBounds(150, 235, 150, 20);
+		JLabel betaalstatusLabel = new JLabel("Betaalstatus");
+		betaalstatusLabel.setBounds(40, 235, 120, 20);
+		
+		mainPanel.add(idLabel);
+		mainPanel.add(idVeld);
+		mainPanel.add(voornaamLabel);
+		mainPanel.add(voornaamVeld);
+		mainPanel.add(tussenvoegselLabel);
+		mainPanel.add(tussenvoegselVeld);
+		mainPanel.add(achternaamLabel);
+		mainPanel.add(achternaamVeld);
+		mainPanel.add(rekeningnrLabel);
+		mainPanel.add(rekeningVeld);
+		mainPanel.add(betaalstatusLabel);
+		mainPanel.add(betaalStatusBox);
+		
 		this.setVisible(true);
+	}
+
+	public void opslaan()
+	{
+		System.out.println("Opslaanknop ingedrukt.");
 		
 	}
 	
 	public static void main(String[] args) 
 	{
-		new KlantView();
+		new KlantView(null, null);
 	}
 }
