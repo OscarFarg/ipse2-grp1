a8a603c8b753b3df85bf9fa48c08bc2abca7039c
l.oversloot <l.oversloot@ec432c0b-3f78-e2e4-c668-383c0ae341a8>
2010-11-05 10:44:18 +0000

    test

--- trunk/src/ipse/BestellingView.java
+++ trunk/src/ipse/BestellingView.java
@@ -1,5 +1,9 @@
 package ipse;
 
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.swing.*;
 
 public class BestellingView extends View
@@ -8,6 +12,7 @@ public class BestellingView extends View
 	betaalDatumVeld, klantIdVeld, medewerkerIdVeld;
 	private JLabel bsLabel, bestellingNrLabel, bestellingDatumLabel, leverDatumLabel, 
 	betaalDatumLabel, klantIdLabel, medewerkerIdLabel;
+	private JComboBox klantBox, medewerkerBox;
 
 
 	public BestellingView( Database database, Controller controller )
@@ -45,39 +50,103 @@ public class BestellingView extends View
 		klantIdLabel = new JLabel( "Klant ID" );
 		klantIdLabel.setBounds( 50, 280, 100, 20 );
 
-		klantIdVeld = new JTextField( 5 );
-		klantIdVeld.setBounds( 150, 280, 200, 20 );
+		klantBox = new JComboBox();
+		try
+		{
+			ResultSet klantSet = database.selectKlantBestelling();
+			while(klantSet.next())
+			{
+				klantBox.addItem(klantSet.getString(1) + ", " + klantSet.getString(2));
+			}
+		}
+		catch( SQLException se )
+		{
+			System.out.println( se );
+		}
+		
+		klantBox.setBounds( 150, 280, 200, 20 );
 
 		medewerkerIdLabel = new JLabel( "Medewerker ID" );
 		medewerkerIdLabel.setBounds( 50, 310, 100, 20 );
+		
+		medewerkerBox = new JComboBox();
+		
+		try
+		{
+			ResultSet medewerkerSet = database.selectMedewerkerBestelling();
+			while(medewerkerSet.next())
+			{
+				medewerkerBox.addItem(medewerkerSet.getString(1) + ", " + medewerkerSet.getString(2) );
+			}
+		}
+		catch( SQLException se )
+		{
+			System.out.println( se );
+		}
+		
+		medewerkerBox.setBounds( 150, 310, 200, 20 );
 
-		medewerkerIdVeld = new JTextField( 5 );
-		medewerkerIdVeld.setBounds( 150, 310, 200, 20 );
+
 
 		mainPanel.add( bsLabel );
 		mainPanel.add( bestellingNrVeld );
 		mainPanel.add( bestellingDatumVeld );
 		mainPanel.add( leverDatumVeld );
 		mainPanel.add( betaalDatumVeld );
-		mainPanel.add( klantIdVeld );
-		mainPanel.add( medewerkerIdVeld );
 		mainPanel.add( bestellingNrLabel );
 		mainPanel.add( bestellingDatumLabel );
 		mainPanel.add( leverDatumLabel );
 		mainPanel.add( betaalDatumLabel );
 		mainPanel.add( klantIdLabel );
 		mainPanel.add( medewerkerIdLabel );
+		mainPanel.add(klantBox);
+		mainPanel.add(medewerkerBox);
 
 		setVisible( true );
 	}
-	
+
+	public BestellingView(Database database, Controller controller, int id)
+	{
+		this(database, controller);
+		updateMode = true;
+		Bestelling bestelling = database.selectBestelling(id);
+		bestellingNrVeld.setText(bestelling.getBestelnr() + "");
+		bestellingDatumVeld.setText(bestelling.getBestelDatum().toString());
+		leverDatumVeld.setText(bestelling.getLeverDatum().toString());
+		klantIdVeld.setText(bestelling.getKlantid() + "");
+		medewerkerIdVeld.setText(bestelling.getMedewerkerid() + "");
+	}
+	/*
 	public void opslaan()
 	{
-		
+		System.out.println("Opslaanknop ingedrukt.");
+		String id = bestellingNrVeld.getText();
+		int bestelNr = 0;
+		try
+		{
+			bestelNr = Integer.parseInt(id);
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+		}
+		Date bestelDatum = bestellingDatumVeld.getText();
+		String voornaam = voornaamVeld.getText();
+		String tussenvoegsel = tussenvoegselVeld.getText();
+		String achternaam = achternaamVeld.getText();
+		String rekeningNr = rekeningVeld.getText();
+		String betaalStatus = (String) betaalStatusBox.getSelectedItem();
+		Klant klant = new Klant(bestelNr, voornaam, tussenvoegsel, achternaam, rekeningNr, betaalStatus, "Actief");
+		if (updateMode)
+		{
+			System.out.println("update");
+			database.updateKlant(klant);
+		}
+		else
+		{
+			System.out.println("insert");
+			database.insertKlant(klant);
+		}
 	}
-	
-	public void annuleren()
-	{
-		
-	}
+	*/
 }
